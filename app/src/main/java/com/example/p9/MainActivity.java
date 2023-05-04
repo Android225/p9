6182c6df8b0a15756e0db1d6295331126c5d1a4d
Andry <a.krishtofor@gmail.com>
2023-05-05 01:38:29 +0300

    инфа из стороннего приложения

--- app/src/main/java/com/example/p9/MainActivity.java
+++ app/src/main/java/com/example/p9/MainActivity.java
@@ -4,11 +4,66 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
+    Button btnSendMessage;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Передача данных в сторонние приложения
+        btnSendMessage = (Button) findViewById(R.id.sendMessage);
+        btnSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = "ТЕКСТ ДЕЛИТСЯ";
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, text);
+                startActivity(Intent.createChooser(intent, "OnShare"));
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Получения данных из стороннего приложения
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                // Обработка текстовых данных
+                String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+                if (sharedText != null) {
+                    TextView text = findViewById(R.id.textView);
+                    text.setText(sharedText);
+                }
+            } else if (type.startsWith("image/")) {
+                // Обработка изображений
+                Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                if (imageUri != null) {
+                    // Здесь будет обрабатываться изображение
+                }
+            }
+        }
+
     }
 }
